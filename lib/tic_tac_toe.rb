51d2d1fef5effed6bc3322f569a6edff9a55a93e
Stacy Johns <stacykjohns@icloud.com>
2020-02-17 21:16:08 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,80 @@
+class TicTacToe
+  attr_reader :board
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+    ]
+
+  def initialize
+    @board = board || Array.new(9, " ")
+  end
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn_count
+    counter = 0
+    @board.each do |token|
+      counter += 1 if token != " "
+    end
+    counter
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn
+    puts "Choose a number between 1-9"
+    index = input_to_index(gets.chomp)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+      #input = gets.chomp
+    end
+  end
+
+  def won?
+    winner = nil
+    WIN_COMBINATIONS.each do |combo|
+      if combo.all? {|win| @board[win] == "X"}
+        winner = combo
+      elsif combo.all? {|win| @board[win] == "O"}
+        winner = combo
+      else
+        winner
+      end
+    end
+    winner
+  end
+end
